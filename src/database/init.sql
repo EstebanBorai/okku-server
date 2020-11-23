087c98f023981dcf5104d9664ad331928128ad03
Esteban Borai <estebanborai@gmail.com>
2020-11-22 21:03:47 -0300

    add: "avatars" table

--- src/database/init.sql
+++ src/database/init.sql
@@ -16,3 +16,13 @@ CREATE TABLE IF NOT EXISTS secrets (
   updated_at timestamp with time zone  NOT NULL  DEFAULT current_timestamp,
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+CREATE TABLE IF NOT EXISTS avatars (
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  image bytea,
+  user_id uuid,
+  mime_type VARCHAR(120),
+  created_at timestamp with time zone  NOT NULL  DEFAULT current_timestamp,
+  updated_at timestamp with time zone  NOT NULL  DEFAULT current_timestamp,
+  FOREIGN KEY(user_id) REFERENCES users(id)
+);
